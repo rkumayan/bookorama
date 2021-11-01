10d9ffcc727e1264a1dcdd5a5fbe1eee981e084a
rahul <kumayanrahul01@gmail.com>
2021-11-01 17:21:05 +0530

    added script to 'add-book' button

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ let del = document.querySelectorAll(".parent-container #container #book-title #d
     });
 
     
+    function addHTML(){
+        let bookName = document.getElementById("add-book").value;
+        if( bookName == "")
+            alert(" Enter a book name first :( ");
+        else{
+            document.getElementById("add-book").value = "";
+            // create all the required elements
+            let li = document.createElement("li");
+            let btn = document.createElement("button");
+
+            // add classes to created elements
+            li.id="book-title";
+            btn.id="delete";
+            //insert content to the created tags
+            btn.textContent="delete";
+            li.textContent=bookName;
+            
+            //append the elements
+            li.appendChild(btn);
+            container.appendChild(li);
+            
+            //adding delete button functionality to above created book-titles
+            container = document.getElementById("container");
+            del = document.querySelectorAll(".parent-container #container #book-title #delete");
+            Array.from(del).forEach( function(val){
+                val.addEventListener('click' , function(){
+                    this.parentElement.remove();
+                })  
+            });
+            
+        } 
+         
+    }
+    
+
